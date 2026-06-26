5862b43c1e9c0652212ced4a6791844a069c88f3
Yeju Suh <yejusuh99@gmail.com>
2026-06-26 15:24:12 -0400

    DB misc

--- constants.py
+++ constants.py
@@ -10,7 +10,7 @@ else:
     # Running as a plain Python script
     _DIR = os.path.dirname(os.path.abspath(__file__))
 
-DB_PATH = os.path.join(_DIR, "parish.db")
+DB_PATH = os.path.join(_DIR, "parish.bak.db")
 _ARROW_SVG   = os.path.join(_DIR, "arrow_down.svg").replace("\\", "/")
 
 C = dict(
